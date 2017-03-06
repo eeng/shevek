f1dd74ae5b6a30d26e9f53403f7dd2b162ccd0f9
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-03-06 19:25:18 -0300

    Queries para traer dimensiones y measures

--- src/clj/pivot/druid.clj
+++ src/clj/pivot/druid.clj
@@ -6,4 +6,28 @@
 (defn datasources [host]
   (:body (http/get (str host "/datasources") {:as :json})))
 
-(datasources broker)
+; TODO en el :context de la q se le puede pasar un timeout
+(defn- query [host q]
+  (:body (http/post (str host "/") {:content-type :json :form-params q :as :json})))
+
+(defn- druid-column-result-to-map [[column fields]]
+  (merge (select-keys fields [:type :cardinality])
+         {:name (name column)}))
+
+(defn dimensions [host datasource]
+  (let [q {:queryType "segmentMetadata"
+           :dataSource {:type "table" :name datasource}
+           :merge true}]
+    (->> (query host q) first :columns (map druid-column-result-to-map))))
+
+(defn measures [host datasource]
+  (let [q {:queryType "segmentMetadata"
+           :dataSource {:type "table" :name datasource}
+           :merge true
+           :analysisTypes ["aggregators"]}]
+    (->> (query host q) first :aggregators (map druid-column-result-to-map))))
+
+#_(datasources broker)
+#_(dimensions broker "vtol_stats")
+#_(dimensions broker "wikiticker")
+#_(measures broker "vtol_stats")
