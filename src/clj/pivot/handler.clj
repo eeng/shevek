d2f1e61ca97e76acf32e56ae32769d3da35e8235
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-03-07 15:21:28 -0300

    Error 404

--- src/clj/pivot/handler.clj
+++ src/clj/pivot/handler.clj
@@ -1,10 +1,13 @@
 (ns pivot.handler
   (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [files not-found]]))
+            [compojure.route :refer [resources not-found]]
+            [clojure.java.io :as io]))
 
 (defroutes app-routes
-  (GET "/" [] "Res"))
+  (GET "/" [] "Res")
+  (resources "/")
+  (not-found (-> "404.html" io/resource slurp)))
 
 ; TODO no se si hará falta el wrap-defaults
 (def app (wrap-defaults app-routes site-defaults))
