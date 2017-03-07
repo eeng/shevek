9ded5fe1a7d42413222243064d20fe0f8abe51a9
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-03-06 21:13:40 -0300

    Coloreo los iconitos de los cubos

--- src/cljs/pivot/dashboard.cljs
+++ src/cljs/pivot/dashboard.cljs
@@ -7,17 +7,20 @@
 
 #_(def cubes (r/atom []))
 
+(defonce colors ["blue" "orange" "green" "olive" "teal" "red"
+                 "purple" "yellow" "violet" "brown" "pink" "grey"])
+
 (defn page []
   [:div
    [:h1.ui.dividing.header "Cubos de Datos"]
    [:div.ui.cards
     (if (seq @cubes)
-      (for [{:keys [name title description]} @cubes]
-        ^{:key name}
+      (for [[i {:keys [name title description]}] (map-indexed vector @cubes)]
+        ^{:key i}
         [:a.card {:href (str "/cubes/" name)}
          [:div.content
           [:div.ui.header
-           [:i.cube.blue.icon]
+           [:i.cube.icon {:class (colors i)}]
            [:div.content title]]
           [:div.description description]]])
       [:div.ui.basic.segment "No hay cubos definidos."])]])
