bbdc0555d3ee250e313ba82bbe5b87548dbf6642
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-03-19 16:13:05 -0300

    Muestro popups al clickear filters

--- src/cljs/pivot/cube_view/filter_split.cljs
+++ src/cljs/pivot/cube_view/filter_split.cljs
@@ -5,18 +5,28 @@
             [pivot.dw :as dw]
             [pivot.lib.react :refer [rmap]]
             [pivot.rpc :as rpc]
-            [pivot.cube-view.shared :refer [panel-header cube-view]]))
+            [pivot.cube-view.shared :refer [panel-header cube-view]]
+            [pivot.components :refer [popup outside-deselectable]]))
 
-(defn- filter-item [{:keys [title] :as dim}]
-  [:button.ui.green.compact.button {:class (when-not (dw/time-dimension? dim) "right labeled icon")}
-   (when-not (dw/time-dimension? dim)
-     [:i.close.icon {:on-click #(dispatch :dimension-removed-from-filter dim)}])
-   title])
+(defn- filter-popup [selected dim]
+  [:div.ui.special.popup.card {:style {:display (if @selected "block" "none")}}
+   [:div.content "popup for" (str dim)]])
+
+(defn- filter-item [selected {:keys [title] :as dim}]
+  [popup
+   [:button.ui.green.compact.button.item
+    {:class (when-not (dw/time-dimension? dim) "right labeled icon")
+     :on-click #(swap! selected not)}
+    (when-not (dw/time-dimension? dim)
+      [:i.close.icon {:on-click #(dispatch :dimension-removed-from-filter dim)}])
+    title]
+   [filter-popup selected dim]
+   {:on "manual" :position "bottom center"}])
 
 (defn filter-panel []
   [:div.filter.panel
    [panel-header (t :cubes/filter)]
-   (rmap filter-item (cube-view :filter))])
+   (rmap (outside-deselectable filter-item) (cube-view :filter))])
 
 (defn- split-item [{:keys [title] :as dim}]
   [:button.ui.orange.compact.right.labeled.icon.button
