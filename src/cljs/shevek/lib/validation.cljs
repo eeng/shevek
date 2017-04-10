b7dc5ec5aa882ae4ff0f9bf61c7448f5d52283a3
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-04-10 11:47:52 -0300

    Refactorizo. Agrego la opt optional?

--- src/cljs/shevek/lib/validation.cljs
+++ src/cljs/shevek/lib/validation.cljs
@@ -5,21 +5,8 @@
 (defn- translate [msg]
   (if (string? msg) msg (t msg)))
 
-(defn pred [predicate {:keys [msg]}]
-  (fn [field record]
-    (let [value (get record field)]
-      (when-not (predicate value)
-        (str/format (translate msg) value)))))
-
-(def required (pred (comp seq str/trim str) {:msg :validation/required}))
-
-(defn regex [pattern & [opts]]
-  (pred #(or (nil? %) (re-find pattern %)) (merge {:msg :validation/regex} opts)))
-
-(def email (regex #"(?i)\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$" {:msg :validation/email}))
-
 (defn- apply-validator [record [field validator]]
-  (if-let [msg (validator field record)]
+  (if-let [msg (validator record field)]
     (update-in record [:errors field] (fnil conj []) msg)
     record))
 
@@ -37,3 +24,20 @@
     (if errors
       (do (swap! atom assoc :errors errors) false)
       true)))
+
+;; Validators
+
+(defn pred [predicate {:keys [msg optional?] :or {optional? true}}]
+  (fn [record field]
+    (let [value (get record field)
+          valid? (or (and optional? (nil? value))
+                     (predicate value))]
+      (when-not valid?
+        (str/format (translate msg) value)))))
+
+(def required (pred (comp seq str/trim str) {:msg :validation/required :optional? false}))
+
+(defn regex [pattern & [opts]]
+  (pred (partial re-find pattern) (merge {:msg :validation/regex} opts)))
+
+(def email (regex #"(?i)\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$" {:msg :validation/email}))
--- test/cljs/shevek/lib/validation_test.cljs
+++ test/cljs/shevek/lib/validation_test.cljs
@@ -4,11 +4,10 @@
             [shevek.lib.validation :refer [validate pred required regex email]]))
 
 (deftest validation-tests
-  (testing "pred validator"
+  (testing "general rules"
     (testing "single validator per field"
       (are [ee vr] (= ee (:errors vr))
         {:x ["pos"]} (validate {:x 0} {:x (pred pos? {:msg "pos"})})
-        {:x ["pos"]} (validate {:x nil} {:x (pred pos? {:msg "pos"})})
         nil (validate {:x 1} {:x (pred pos? {:msg "pos"})})))
 
     (testing "multiple validators per field"
@@ -16,26 +15,37 @@
         {:x ["> 0" "> 1"]} (validate {:x 0} {:x [(pred #(> % 0) {:msg "> 0"})
                                                  (pred #(> % 1) {:msg "> 1"})]})))
 
-    (testing "value interpolation"
+    (testing "validators are optional by default"
+      (are [ee vr] (= ee (:errors vr))
+        nil (validate {:x nil} {:x (pred pos? {:msg "pos"})})
+        {:x ["pos"]} (validate {:x nil} {:x (pred pos? {:msg "pos" :optional? false})})))
+
+    (testing "value interpolation in the message"
       (are [ee vr] (= ee (:errors vr))
         {:x ["val 'foo' is invalid"]}
         (validate {:x "foo"} {:x (pred #(= % "bar") {:msg "val '%s' is invalid"})}))))
 
-  (testing "required validator"
-    (are [ee vr] (= ee (:errors vr))
-      {:x ["can't be blank"]} (validate {} {:x required})
-      {:x ["can't be blank"]} (validate {:x " "} {:x required})
-      nil (validate {:x "..."} {:x required})))
+  (testing "validators"
+    (testing "required validator"
+      (are [ee vr] (= ee (:errors vr))
+        {:x ["can't be blank"]} (validate {} {:x required})
+        {:x ["can't be blank"]} (validate {:x " "} {:x required})
+        nil (validate {:x "..."} {:x required})))
 
-  (testing "regex validator"
-    (are [ee vr] (= ee (:errors vr))
-      nil (validate {:x "foo"} {:x (regex #"foo")})
-      {:x ["doesn't match pattern"]} (validate {:x "bar"} {:x (regex #"foo")})
-      {:x ["oops"]} (validate {:x "bar"} {:x (regex #"foo" {:msg "oops"})})
-      nil (validate {:x nil} {:x (regex #"foo")})
-      {:x ["doesn't match pattern"]} (validate {:x ""} {:x (regex #"foo")})))
+    (testing "regex validator"
+      (are [ee vr] (= ee (:errors vr))
+        nil (validate {:x "foo"} {:x (regex #"foo")})
+        {:x ["doesn't match pattern"]} (validate {:x "bar"} {:x (regex #"foo")})
+        {:x ["oops"]} (validate {:x "bar"} {:x (regex #"foo" {:msg "oops"})})
+        nil (validate {:x nil} {:x (regex #"foo")})
+        {:x ["doesn't match pattern"]} (validate {:x ""} {:x (regex #"foo")})))
 
-  (testing "email validator"
-    (are [ee vr] (= ee (:errors vr))
-      {:x ["is not a valid email address"]} (validate {:x "foo"} {:x email})
-      nil (validate {:x "foo@bar.com"} {:x email}))))
+    (testing "email validator"
+      (are [ee vr] (= ee (:errors vr))
+        {:x ["is not a valid email address"]} (validate {:x "foo"} {:x email})
+        nil (validate {:x "foo@bar.com"} {:x email})))
+
+    (testing "email validator"
+      (are [ee vr] (= ee (:errors vr))
+        {:x ["is not a valid email address"]} (validate {:x "foo"} {:x email})
+        nil (validate {:x "foo@bar.com"} {:x email})))))
