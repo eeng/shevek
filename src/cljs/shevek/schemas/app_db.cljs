e0f6fa7dd2a24fe61e73832a7275a04426e0701d
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-04-11 15:07:33 -0300

    Quito la duplicacion del app_db

--- src/cljs/shevek/schemas/app_db.cljs
+++ src/cljs/shevek/schemas/app_db.cljs
@@ -1,21 +1,11 @@
 (ns shevek.schemas.app-db
-  (:require [schema.core :as s])
+  (:require [schema.core :as s]
+            [shevek.schemas.cube :refer [Dimension Measure]]
+            [shevek.schemas.viewer :as vs])
   (:import goog.date.DateTime))
 
 (s/defschema Settings
   {:lang s/Str})
-
-; TODO desde aca hasta el viewer esta duplicado en el server
-(s/defschema NTD
-  {:name s/Str
-   :title s/Str
-   (s/optional-key :description) (s/maybe s/Str)})
-
-(s/defschema Dimension
-  (assoc NTD :type s/Str))
-
-(s/defschema Measure
-  (assoc NTD :type s/Str))
 
 (s/defschema Cube
   {(s/optional-key :_id) s/Any
@@ -26,42 +16,8 @@
    (s/optional-key :measures) [Measure]
    (s/optional-key :max-time) goog.date.DateTime})
 
-(s/defschema SortBy
-  (assoc Dimension :descending s/Bool))
-
-(s/defschema Split
-  (assoc Dimension
-         :limit (s/cond-pre s/Int s/Str)
-         (s/optional-key :sort-by) SortBy
-         (s/optional-key :granularity) s/Str))
-
-(s/defschema TimeFilter
-  (assoc Dimension
-         :name (s/eq "__time")
-         :selected-period s/Keyword))
-
-(s/defschema NormalFilter
-  (assoc Dimension
-         :operator s/Str
-         (s/optional-key :value) #{(s/maybe s/Str)}))
-
-(s/defschema Filter
-  (s/if :selected-period TimeFilter NormalFilter))
-
-(s/defschema Pinboard
-  {:measure Measure :dimensions [Split]})
-
-(s/defschema Result {s/Keyword s/Any})
-
 (s/defschema Viewer
-  {:cube Cube
-   (s/optional-key :filter) [(s/one TimeFilter "tf") NormalFilter]
-   (s/optional-key :split) [Split]
-   (s/optional-key :arrived-split) [Split]
-   (s/optional-key :measures) [Measure]
-   (s/optional-key :pinboard) Pinboard
-   (s/optional-key :results) {(s/enum :main :pinboard :filter) (s/cond-pre [Result] {s/Str [Result]})}
-   (s/optional-key :last-added-filter) s/Any})
+  (assoc vs/Viewer :cube Cube))
 
 (s/defschema AppDB
   {(s/optional-key :page) s/Keyword
