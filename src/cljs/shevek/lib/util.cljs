bf9b3d9f9c1d9679e320429bf39304b7989280ef
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-04-15 20:43:05 -0300

    Hago un variable-debounce para poder pasarle distintos timeouts en cada llamada

--- src/cljs/shevek/lib/util.cljs
+++ src/cljs/shevek/lib/util.cljs
@@ -1,11 +1,18 @@
 (ns shevek.lib.util)
 
-(defn debounce [f wait]
+(defn variable-debounce
+  "Allows to debounce a function with a different timeout on each call"
+  [f]
   (let [timeout (atom nil)]
-    (fn [& args]
+    (fn [& wait args]
       (let [later #(do (reset! timeout nil) (apply f args))]
         (when @timeout (js/clearTimeout @timeout))
         (reset! timeout (js/setTimeout later wait))))))
+
+(defn debounce
+  "Calls f after 'wait' milliseconds, but if another call is made before that timeout it will be recreate the timeout"
+  [f wait]
+  (partial (variable-debounce f) wait))
 
 (defn every [secs f]
   (js/setInterval f (* 1000 secs)))
--- src/cljs/shevek/notification.cljs
+++ src/cljs/shevek/notification.cljs
@@ -1,14 +1,17 @@
 (ns shevek.notification
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [shevek.lib.util :refer [variable-debounce]]))
 
-(defonce notification-data (r/atom {}))
+(defonce state (r/atom {}))
+
+(def hide-after (variable-debounce #(swap! state assoc :showed? false)))
 
 (defn notify [message & {:keys [timeout] :or {timeout 5000} :as opts}]
-  (reset! notification-data (assoc opts :message message :showed? true))
-  (js/setTimeout #(swap! notification-data assoc :showed? false) timeout))
+  (reset! state (assoc opts :message message :showed? true))
+  (hide-after timeout))
 
 (defn notification []
-  (let [{:keys [showed? message]} @notification-data]
+  (let [{:keys [showed? message]} @state]
     [:div#notification
      {:class (if showed? "visible" "hidden")}
      message]))
