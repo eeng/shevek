110ebc77eae8383c7f32d4127ffcddb7d96bd5d4
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-07-06 15:57:35 -0300

    Tokens expire after one day

--- src/clj/shevek/lib/auth.clj
+++ src/clj/shevek/lib/auth.clj
@@ -3,14 +3,21 @@
             [shevek.db :refer [db]]
             [shevek.users.repository :as users]
             [shevek.config :refer [config]]
-            [bcrypt-clj.auth :refer [check-password]]))
+            [bcrypt-clj.auth :refer [check-password]]
+            [clj-time.core :as t]))
+
+(defn- generate-token [{:keys [username]}]
+  (jwt/sign {:username username :exp (t/plus (t/now) (t/days 1))} (config :jwt-secret)))
 
 (defn authenticate [db {:keys [username password]}]
   (let [user (users/find-by-username db username)]
     (if (and user (check-password password (:password user)))
-      {:status :success :token (jwt/sign {:username username} (config :jwt-secret))}
-      {:status :failure :error :invalid-credentials})))
+      {:token (generate-token user)}
+      {:error :invalid-credentials})))
 
 (defn login [params]
-  (let [{:keys [status] :as res} (authenticate db params)]
-    {:status (if (= status :success) 201 401) :body res}))
+  (let [{:keys [token] :as res} (authenticate db params)]
+    {:status (if token 201 401) :body res}))
+
+#_(def token (:token (authenticate db {:username "emma" :password "asdf654"})))
+#_(jwt/unsign token (config :jwt-secret))
--- test/clj/shevek/lib/auth_test.clj
+++ test/clj/shevek/lib/auth_test.clj
@@ -7,18 +7,17 @@
             [shevek.makers :refer [make!]]
             [shevek.users.repository :refer [User]]
             [shevek.db :refer [db]]
-            [buddy.sign.jwt :as jwt]))
+            [buddy.sign.jwt :as jwt]
+            [clj-time.core :as t]))
 
 (deftest authenticate-tests
   (it "valid credentials should return a JSON Web Token"
     (make! User {:username "john" :password "secret123"})
-    (let [{:keys [status token]} (authenticate db {:username "john" :password "secret123"})]
-      (is (= :success status))
+    (let [{:keys [token]} (authenticate db {:username "john" :password "secret123"})]
       (is (submap? {:username "john"}  (jwt/unsign token (config :jwt-secret))))))
 
   (it "non-existent user should return an error message"
-    (let [{:keys [status error]} (authenticate db {:username "john"})]
-      (is (= :failure status))
+    (let [{:keys [error]} (authenticate db {:username "john"})]
       (is (= :invalid-credentials error))))
 
   (it "invalid credentials user should return an error message"
@@ -29,4 +28,10 @@
       :invalid-credentials {:username "John" :password "secret123"}
       :invalid-credentials {:username "" :password "secret123"}
       :invalid-credentials {:username "john" :password ""}
-      nil {:username "john" :password "secret123"})))
+      nil {:username "john" :password "secret123"}))
+
+  (it "tokens are valid for one day"
+    (make! User {:username "john" :password "secret123"})
+    (let [{:keys [token]} (authenticate db {:username "john" :password "secret123"})]
+      (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Token is expired"
+           (jwt/unsign token (config :jwt-secret) {:now (t/plus (t/now) (t/hours 25))}))))))
