7cb5620da740c797aac162d89b282c12d10f4fd4
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-07-10 14:58:49 -0300

    Better drag image

--- src/cljs/shevek/components/drag_and_drop.cljs
+++ src/cljs/shevek/components/drag_and_drop.cljs
@@ -2,13 +2,19 @@
   (:require [cljs.reader :refer [read-string]]))
 
 (defn drag-start [event transfer-data]
-  (.. event -dataTransfer (setData "application/x-clojure-data" transfer-data)))
+  (.. event -dataTransfer (setData "application/x-clojure-data" transfer-data))
+  (let [drag-image (-> (js/$ "<div class='drag-image'/>")
+                       (.text (.. event -target -innerText))
+                       (.appendTo "body")
+                       (.get 0))]
+    (.. event -dataTransfer (setDragImage drag-image -10 20))))
 
 (defn handle-drop [handler]
   (fn [event]
     (let [transfer-data (read-string (.. event -dataTransfer (getData "application/x-clojure-data")))]
       (handler transfer-data)
-      (.preventDefault event))))
+      (.preventDefault event)
+      (.remove (js/$ ".drag-image")))))
 
 (defn drag-over [event]
   (.preventDefault event))
