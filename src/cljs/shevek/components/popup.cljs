2e32a986e8bf9a396cc10ae1281a0055ad323495
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-07-16 15:24:09 -0300

    No need for the css pointer-events if we use currentTarget

--- src/cljs/shevek/components/popup.cljs
+++ src/cljs/shevek/components/popup.cljs
@@ -1,17 +1,18 @@
 (ns shevek.components.popup
   (:require [reagent.core :as r]))
 
-(def popup-data (r/atom {}))
+(def popup-data (r/atom {:opened? false}))
 
 (defn show-popup [event new-content {:keys [on-close id] :or {on-close identity} :as popup-opts}]
   (let [{:keys [opened? content activator]} @popup-data]
-    (reset! popup-data {:opened? true :activator (.-target event) :content new-content
+    (reset! popup-data {:opened? true :activator (.-currentTarget event) :content new-content
                         :on-close on-close :id id
                         :js-opts (dissoc popup-opts :on-close :id)})))
 
 (defn close-popup []
   (swap! popup-data assoc :opened? false)
-  ((@popup-data :on-close)))
+  (let [{:keys [on-close]} @popup-data]
+    (when on-close (on-close))))
 
 (defn popup-opened? [id]
   (and (@popup-data :opened?) (= id (@popup-data :id))))
@@ -38,7 +39,9 @@
         handle-click-outside (fn [container event]
                                (when (and (@popup-data :opened?)
                                           (not (.contains (r/dom-node container) (.-target event))))
-                                 (when (= (@popup-data :activator) (.-target event))
+                                 (when (and (.contains (@popup-data :activator) (.-target event))
+                                            (not (-> event .-target js/$ (.is ".close.icon"))))
+                                   ; Without this a click on the same activator would close the popup first as is considered outside and then it would trigger the show-popup again
                                    (.stopPropagation event))
                                  (close-popup)))]
     (r/create-class {:reagent-render popup*
