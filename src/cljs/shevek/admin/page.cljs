36d25eef2dad2332f422ad665fb6d09d38f0a0bd
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-07-24 12:26:30 -0300

    Removed schema-section from the UI as we will using the seed file for now

--- src/cljs/shevek/admin/page.cljs
+++ src/cljs/shevek/admin/page.cljs
@@ -1,11 +1,9 @@
 (ns shevek.admin.page
   (:require [shevek.i18n :refer [t]]
             [shevek.components.text :refer [page-title]]
-            [shevek.admin.users :refer [users-section]]
-            [shevek.admin.schema :refer [schema-section]]))
+            [shevek.admin.users :refer [users-section]]))
 
 (defn page []
  [:div#settings.ui.container
   [page-title (t :admin/title) (t :admin/subtitle) "users"]
-  [users-section]
-  [schema-section]])
+  [users-section]])
