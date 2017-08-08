3bc407d6503d187e756c741fb13b88f96f889ccd
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-08-08 13:26:45 -0300

    Pie chart working

--- src/cljs/shevek/viewer/chart.cljs
+++ src/cljs/shevek/viewer/chart.cljs
@@ -10,31 +10,44 @@
       shuffle
       cycle))
 
-(defn- build-dataset [{:keys [title] :as measure} results measures colors]
+(defn- build-dataset [{:keys [title] :as measure} results measures viztype colors]
   {:label title
    :data (map #(dimension-value measure %) results)
-   :backgroundColor (if (= (count measures) 1)
+   :backgroundColor (if (or (= (count measures) 1) (= viztype :pie-chart))
                       (take (count results) colors)
                       (nth colors (index-of measures measure)))})
 
-(defn- viewer->chart-data [{:keys [measures results]} colors]
+(defn- viewer->chart-data [{:keys [measures results viztype] :as viewer} colors]
   (let [split (:split results)
         results (rest (:main results)) ; We don't need the totals row
         labels (map #(format-dimension (first split) %) results) ; TODO funca para un solo split x ahora
-        datasets (map #(build-dataset % results measures colors) measures)]
+        datasets (map #(build-dataset % results measures viztype colors) measures)]
     {:labels labels :datasets datasets}))
 
 (def chart-types {:bar-chart "bar"
                   :line-chart "line"
                   :pie-chart "pie"})
 
+; This two guys will make pie tooltips look like bar tooltips, as the default ones lack the dataset labels (our measures)
+(defn- pie-tooltip-title [tooltip-items data]
+  (get (.-labels data) (.-index (first tooltip-items))))
+
+(defn- pie-tooltip-label [tooltip-item data]
+  (let [ds (get (.-datasets data) (.-datasetIndex tooltip-item))
+        value (get (.-data ds) (.-index tooltip-item))]
+    (str (.-label ds) ": " value)))
+
 (defn- build-chart [canvas {:keys [viztype] :as viewer}]
-  (let [colors (make-color-palette)]
+  (let [colors (make-color-palette)
+        options (case viztype
+                  :bar-chart {:scales {:yAxes [{:ticks {:beginAtZero true}}]}}
+                  :pie-chart {:tooltips {:callbacks {:title pie-tooltip-title :label pie-tooltip-label}}}
+                  {})]
     {:colors colors
      :object (js/Chart. canvas
                         (clj->js {:type (chart-types viztype)
                                   :data (viewer->chart-data viewer colors)
-                                  :options {:scales {:yAxes [{:ticks {:beginAtZero true}}]}}}))}))
+                                  :options options}))}))
 
 
 (defn- update-chart [{:keys [object colors]} viewer]
