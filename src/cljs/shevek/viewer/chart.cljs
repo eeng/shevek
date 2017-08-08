99f31bb17318c62bd56b2567307c67926db8b0f4
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-08-08 16:35:01 -0300

    Didn't like the charts changing colors all the time

--- src/cljs/shevek/viewer/chart.cljs
+++ src/cljs/shevek/viewer/chart.cljs
@@ -4,12 +4,11 @@
             [shevek.viewer.shared :refer [format-dimension dimension-value]]
             [shevek.lib.collections :refer [index-of]]))
 
-(defn make-color-palette []
-  (-> ["#ef5350" "#ec407a" "#ab47bc" "#7e57c2" "#5c6bc0" "#42a5f5" "#81d4fa" "#26c6da" "#26a69a"
-       "#66bb6a" "#9ccc65" "#d4e157" "#ffee58" "#ffca28" "#ffa726" "#ff7043" "#8d6e63" "#78909c"]
-      shuffle cycle))
+(def colors
+  (cycle ["#ef5350" "#ec407a" "#ab47bc" "#7e57c2" "#5c6bc0" "#42a5f5" "#81d4fa" "#26c6da" "#26a69a"
+          "#66bb6a" "#9ccc65" "#d4e157" "#ffee58" "#ffca28" "#ffa726" "#ff7043" "#8d6e63" "#78909c"]))
 
-(defn- viztype-dependant-dataset-opts [viztype results measure measures colors]
+(defn- viztype-dependant-dataset-opts [viztype results measure measures]
   (case viztype
     :line-chart {:fill false
                  :borderColor (nth colors (index-of measures measure))}
@@ -18,16 +17,16 @@
                         (take (count results) colors)
                         (nth colors (index-of measures measure)))}))
 
-(defn- build-dataset [{:keys [title] :as measure} results measures viztype colors]
+(defn- build-dataset [{:keys [title] :as measure} results measures viztype]
   (merge {:label title
           :data (map #(dimension-value measure %) results)}
-         (viztype-dependant-dataset-opts viztype results measure measures colors)))
+         (viztype-dependant-dataset-opts viztype results measure measures)))
 
-(defn- viewer->chart-data [{:keys [measures results viztype] :as viewer} colors]
+(defn- viewer->chart-data [{:keys [measures results viztype] :as viewer}]
   (let [split (:split results)
         results (rest (:main results)) ; We don't need the totals row
         labels (map #(format-dimension (first split) %) results) ; TODO funca para un solo split x ahora
-        datasets (map #(build-dataset % results measures viztype colors) measures)]
+        datasets (map #(build-dataset % results measures viztype) measures)]
     {:labels labels :datasets datasets}))
 
 (def chart-types {:bar-chart "bar"
@@ -44,24 +43,22 @@
     (str (.-label ds) ": " value)))
 
 (defn- build-chart [canvas {:keys [viztype] :as viewer}]
-  (let [colors (make-color-palette)
-        options (case viztype
+  (let [options (case viztype
                   :pie-chart {:tooltips {:callbacks {:title pie-tooltip-title :label pie-tooltip-label}}}
                   {:scales {:yAxes [{:ticks {:beginAtZero true}}]}})]
-    {:colors colors
-     :object (js/Chart. canvas
-                        (clj->js {:type (chart-types viztype)
-                                  :data (viewer->chart-data viewer colors)
-                                  :options options}))}))
+    (js/Chart. canvas
+               (clj->js {:type (chart-types viztype)
+                         :data (viewer->chart-data viewer)
+                         :options options}))))
 
 
-(defn- update-chart [{:keys [object colors]} viewer]
-  (aset object "data" (clj->js (viewer->chart-data viewer colors)))
-  (.update object))
+(defn- update-chart [chart viewer]
+  (aset chart "data" (clj->js (viewer->chart-data viewer)))
+  (.update chart))
 
 (defn chart-visualization [viewer]
   (let [chart (atom nil)]
     (r/create-class {:reagent-render (fn [_] [:canvas])
                      :component-did-mount #(reset! chart (build-chart (r/dom-node %) viewer))
                      :component-did-update #(update-chart @chart (r/props %))
-                     :component-will-unmount #(do (.destroy (:object @chart)) (reset! chart nil))})))
+                     :component-will-unmount #(do (.destroy @chart) (reset! chart nil))})))
