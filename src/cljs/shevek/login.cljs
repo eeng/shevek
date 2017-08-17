2ddf770c2186a8ca5d1efd3b02fe6cf872edbb5c
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-08-17 16:41:55 -0300

    Refactor login to use input-field as now supports icons

--- src/cljs/shevek/login.cljs
+++ src/cljs/shevek/login.cljs
@@ -5,7 +5,7 @@
             [shevek.reflow.db :as db]
             [shevek.reflow.core :refer [dispatch]]
             [shevek.rpc :as rpc]
-            [shevek.components.form :refer [text-input kb-shortcuts]]
+            [shevek.components.form :refer [text-input input-field kb-shortcuts]]
             [shevek.lib.session-storage :as session-storage]
             [ajax.core :refer [POST]]
             [cljsjs.jwt-decode]))
@@ -57,28 +57,24 @@
 (defevh :user-restored [db]
   (assoc db :current-user (extract-user (session-storage/get-item "shevek.access-token"))))
 
-(defn page []
+(defn- login-form []
   (let [user (r/atom {})
         login #(dispatch :login user)
         shortcuts (kb-shortcuts :enter login)]
     (fn []
-      [:div#login.ui.center.aligned.grid
-       [:div.column
-        [:h1.ui.blue.header
-         [:img {:src "images/logo.png"}]
-         [:div.content "Shevek"
-          [:div.sub.header "Data Warehouse Visualization System"]]]
-        [:div.ui.segment (rpc/loading-class :logging-in)
-         [:div.ui.form {:ref shortcuts :class (when @error "error")}
-          [:div.field
-           [:div.ui.left.icon.input
-            [:i.user.icon]
-            [text-input user :username {:placeholder (t :users/username) :auto-focus true}]]]
-          [:div.field
-           [:div.ui.left.icon.input
-            [:i.lock.icon]
-            [text-input user :password {:placeholder (t :users/password) :type "password"}]]]
-          (when @error
-            [:div.ui.error.message
-             (t @error)])
-          [:button.ui.fluid.large.blue.primary.button {:on-click login} "Login"]]]]])))
+      [:div.ui.form {:ref shortcuts :class (when @error "error")}
+       [input-field user :username {:placeholder (t :users/username) :auto-focus true :icon "user"}]
+       [input-field user :password {:placeholder (t :users/password) :type "password" :icon "lock"}]
+       (when @error
+         [:div.ui.error.message (t @error)])
+       [:button.ui.fluid.large.blue.primary.button {:on-click login} "Login"]])))
+
+(defn page []
+  [:div#login.ui.center.aligned.grid
+   [:div.column
+    [:h1.ui.blue.header
+     [:img {:src "images/logo.png"}]
+     [:div.content "Shevek"
+      [:div.sub.header "Data Warehouse Visualization System"]]]
+    [:div.ui.segment (rpc/loading-class :logging-in)
+     [login-form]]]])
