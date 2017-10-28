a1f3be4117d0b6a99574d762d6bfdeb49a39fd3b
Emmanuel Nicolau <emmanicolau@gmail.com>
2017-10-28 19:43:41 -0300

    Upload the config file after the package, because the dist folder gets cleaned on package

--- cap/deploy.rb
+++ cap/deploy.rb
@@ -4,17 +4,8 @@ set :application, "shevek"
 set :deploy_to, "/var/apps/#{fetch :application}"
 set :repo_url, "ssh://dev@test.vitolen.com:14000/git/#{fetch :application}.git"
 set :pty, true
-append :linked_files, "dist/config.edn"
 
 namespace :deploy do
-  desc "Upload Shevek config file"
-  task :upload do
-    on roles(:all) do |host|
-      upload! "cap/files/config.edn", "#{shared_path}/dist/config.edn"
-    end
-  end
-  before 'deploy:check:linked_files', :upload
-
   desc "Generates the jar file"
   task :package do
     on roles(:all) do |host|
@@ -22,5 +13,14 @@ namespace :deploy do
     end
   end
   after :updated, :package
+
+  desc "Upload Shevek config file"
+  task :upload_config do
+    on roles(:all) do |host|
+      upload! "cap/files/config.edn", "#{release_path}/dist/config.edn"
+    end
+  end
+  after :package, :upload_config
+
   after :published, 'service:shevek:restart'
 end
