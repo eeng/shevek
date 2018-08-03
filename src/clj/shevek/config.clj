f32d94ed076dec59eb50b5da1aefc96be0da1fbc
Emmanuel Nicolau <emmanicolau@gmail.com>
2018-08-03 19:38:00 -0300

    Add schema for config

--- src/clj/shevek/config.clj
+++ src/clj/shevek/config.clj
@@ -1,9 +1,13 @@
 (ns shevek.config
   (:require [mount.core :refer [defstate]]
             [cprop.core :refer [load-config]]
+            [schema.core :as s]
+            [shevek.schemas.config :refer [Config]]
             [shevek.lib.collections :refer [wrap-coll]]))
 
-(defstate cfg :start (load-config))
+(defstate cfg
+  :start (s/validate Config (load-config))
+  :stop :stopped) ; Helps with reloading on case of config errors
 
 (defn config
   ([key]
---
+++ src/clj/shevek/schemas/config.clj
@@ -0,0 +1,20 @@
+(ns shevek.schemas.config
+  (:require [schema.core :as s]
+            [cprop.core :refer [load-config]]))
+
+(s/defschema NotificationsConfig
+  {:server {s/Keyword s/Str}
+   :errors {:to s/Str}})
+
+(s/defschema Config
+  {:env s/Keyword
+   :port s/Int
+   :nrepl-port s/Int
+   :mongodb-uri s/Str
+   :druid-uri s/Str
+   :jwt-secret s/Str
+   :datasources-discovery-interval s/Int
+   :time-boundary-update-interval s/Int
+   :log {:level s/Keyword}
+   :notifications NotificationsConfig
+   :cubes s/Any}) ; They will be validated on the repository
