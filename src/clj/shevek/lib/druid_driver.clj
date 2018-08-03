7d6849f7cac3d60d2aa5a71f906ec3e9d13eb9c9
Emmanuel Nicolau <emmanicolau@gmail.com>
2018-08-03 18:53:57 -0300

    Improve error handling when we can't connect to Druid

--- src/clj/shevek/lib/druid_driver.clj
+++ src/clj/shevek/lib/druid_driver.clj
@@ -7,21 +7,23 @@
   (datasources [this])
   (send-query [this q]))
 
-(derive ::druid-error :shevek.app/error)
+(defn- request [method-fn uri opts]
+  (try
+    (:body (method-fn uri opts))
+    (catch java.net.ConnectException e
+      (throw (ex-info "Can't connect to Druid" {:uri uri})))
+    (catch clojure.lang.ExceptionInfo e
+      (throw (ex-info "Druid error" (-> e ex-data :body (parse-string true)))))))
 
 (defrecord Druid [uri]
   DruidDriver
 
   (datasources [_]
-    (:body (http/get (str uri "/druid/v2/datasources") {:as :json :conn-timeout 10000})))
+    (request http/get (str uri "/druid/v2/datasources") {:as :json :conn-timeout 10000}))
 
   (send-query [_ dq]
     (log/debug "Sending query to Druid:" (generate-string dq))
-    (try
-      (:body (http/post (str uri "/druid/v2") {:content-type :json :form-params dq :as :json}))
-      (catch clojure.lang.ExceptionInfo e
-        (throw (ex-info "Druid error" (-> e ex-data :body (parse-string true)
-                                          (assoc :type ::druid-error))))))))
+    (request http/post (str uri "/druid/v2") {:content-type :json :form-params dq :as :json})))
 
 (defn connect [uri]
   (Druid. uri))
--- src/clj/shevek/web/error.clj
+++ src/clj/shevek/web/error.clj
@@ -3,6 +3,7 @@
             [taoensso.timbre :as log]
             [clojure.repl :refer [root-cause]]))
 
+; Queries run within a pmap so if a timeout occours in Druid we will get an ExecutionException with the ExceptionInfo wrapped, hence the root-cause. That way we can translate some errors on the client.
 (defn wrap-server-error [handler]
   (fn [req]
     (try
@@ -10,9 +11,7 @@
       (catch Exception e
         (log/error e)
         (notify-error e {:request (dissoc req :user)})
-        (let [message (if (instance? clojure.lang.ExceptionInfo e)
-                        (-> e root-cause ex-data :error) ; Queries run within a pmap so if a timeout occours in Druid we will get an ExecutionException with the ExceptionInfo wrapped, hence the root-cause. That way we can translate some errors on the client.
-                        (.getMessage e))]
+        (let [message (or (-> e root-cause ex-data :error) (.getMessage e))]
           {:status 500 :body message})))))
 
 (defn client-error [{:keys [params] :as req}]
