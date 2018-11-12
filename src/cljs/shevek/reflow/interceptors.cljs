fbc22e00efdbb5372bcf59ddef1199e17821e1d0
Emmanuel Nicolau <emmanicolau@gmail.com>
2018-11-12 15:24:14 -0300

    Record only the event name for now, in order no to leak sensitive data

--- src/cljs/shevek/reflow/interceptors.cljs
+++ src/cljs/shevek/reflow/interceptors.cljs
@@ -20,7 +20,8 @@
 
 (defn recorder [interceptor & {:keys [events-to-keep] :or {events-to-keep 20}}]
   (fn [db event]
-    (let [recorded-event {:timestamp (js/Date.) :event event}]
+    (let [recorded-event {:timestamp (js/Date.)
+                          :event (first event)}] ; Record only the name of the event for now. If we were to store the whole event with its params we'd have to take care of hiding sensitive data.
       (-> db
           (update :last-events #(->> %1 (cons %2) (take events-to-keep) vec) recorded-event)
           (interceptor event)))))
