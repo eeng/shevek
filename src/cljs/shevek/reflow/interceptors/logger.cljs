b67ed66aca42a859d98866400befec6b0fa17801
Emmanuel Nicolau <emmanicolau@gmail.com>
2018-12-20 22:13:59 -0300

    Add elapsed time to cljs logger

--- src/cljs/shevek/reflow/interceptors/logger.cljs
+++ src/cljs/shevek/reflow/interceptors/logger.cljs
@@ -7,13 +7,18 @@
   (fn [db event]
     (log/info "Handling event" event "...")
     (if debug?
-      (let [new-db (interceptor db event)
+      (let [start-time (js/Date.)
+            new-db (interceptor db event)
+            end-time (js/Date.)
+            elapsed-time (- end-time start-time)
             [only-before only-after] (data/diff
                                       (apply dissoc db keys-excluded-from-diff)
                                       (apply dissoc new-db keys-excluded-from-diff))
-            db-changed? (or (some? only-before) (some? only-after))]
-        (if db-changed?
-          (log/info "Finished event with changes: before" only-before "after" only-after)
-          (log/info "Finished event with no changes."))
+            diff-time (- (js/Date.) end-time)
+            db-changed? (or (some? only-before) (some? only-after))
+            changes (if db-changed?
+                      ["with changes: before" only-before "after" only-after]
+                      ["with no changes."])]
+        (apply log/info "Finished event in" elapsed-time "ms" (conj changes (str "(diff: " diff-time " ms)")))
         new-db)
       (interceptor db event))))
