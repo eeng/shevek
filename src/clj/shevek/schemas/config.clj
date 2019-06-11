5f5a898ff904709f4f47cbf0be0e2948729ef64f
Emmanuel Nicolau <emmanicolau@gmail.com>
2019-06-11 12:11:09 -0300

    Add a condition to session key in schema

--- src/clj/shevek/schemas/config.clj
+++ src/clj/shevek/schemas/config.clj
@@ -8,13 +8,17 @@
 (s/defschema MonitoringConfig
   {(s/optional-key :influx) {s/Keyword s/Str}})
 
+(defn- sixteen-chars? [s]
+  (= (count s) 16))
+
 (s/defschema Config
   {:env s/Str
    :port s/Int
    :nrepl-port s/Int
    :mongodb-uri s/Str
    :druid-uri s/Str
-   :session {:key s/Str :timeout s/Int}
+   :session {:key (s/constrained s/Str sixteen-chars?)
+             :timeout s/Int}
    :datasources-discovery-interval s/Int
    :time-boundary-update-interval s/Int
    :log {:level s/Keyword :timestamp s/Bool :to s/Str}
