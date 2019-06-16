374094d0fd53605b41db465b9ee29ac1be9db7e7
Emmanuel Nicolau <emmanicolau@gmail.com>
2019-06-15 22:15:57 -0300

    Refactor the window calculation into a separate component

--- src/cljs/shevek/components/virtualized.cljs
+++ src/cljs/shevek/components/virtualized.cljs
@@ -8,29 +8,35 @@
         ; client-height (.-clientHeight element)
     (reset! first-window-row (Math/floor (* (/ scroll-top scroll-height) row-count)))))
 
-(defn virtual-table [_]
-  (let [first-window-row (r/atom 0)]
-    (fn [{:keys [class header-count header-renderer row-count row-renderer row-height height window-buffer]
-          :or {header-count 1 window-buffer 10}}]
-      (let [on-scroll (partial handle-scroll first-window-row row-count)
-            full-height (* row-height row-count)
+(defn- window-provider []
+  (let [inner-window-start (r/atom 0)]
+    (fn [{:keys [item-count item-height window-buffer]} render-fn]
+      (let [on-scroll (partial handle-scroll inner-window-start item-count)
+            content-height (* item-height item-count)
             window-size 10 ; TODO RV calculate on mount
-            expanded-window-start (max (- @first-window-row window-buffer) 0)
-            expanded-window-end (min (+ @first-window-row window-size window-buffer) row-count)
-            window (range expanded-window-start expanded-window-end)
-            spacer-height (* expanded-window-start row-height)]
-        [:div.virtual-table {:style {:height height}} ; TODO RV not sure why the height is needed as the parent div already has the height set
-         ; TODO RV los headers se estan rerenderizando en cada scroll event y no hace falta
-         [:div
-          [:table {:class class}
-           (when header-renderer
-             [:thead
-              (for [row-idx (range header-count)]
-                ^{:key row-idx} [header-renderer {:row-idx row-idx}])])]]
-         [:div.scroll-area {:on-scroll on-scroll}
-          [:div {:style {:height full-height}} ; Fake the full content so the scroll remaining is consistent
-           [:div.spacer {:style {:height spacer-height}}]
-           [:table {:class class}
-            [:tbody
-             (for [row-idx window]
-               ^{:key row-idx} [row-renderer {:style {:height row-height} :row-idx row-idx}])]]]]]))))
+            outer-window-start (max (- @inner-window-start window-buffer) 0)
+            outer-window-end (min (+ @inner-window-start window-size window-buffer) item-count)
+            window (range outer-window-start outer-window-end)
+            spacer-height (* outer-window-start item-height)]
+        [:div.scroll-area {:on-scroll on-scroll}
+         [:div {:style {:height content-height}} ; Fake the full content so the scroll remaining is consistent
+          [:div.spacer {:style {:height spacer-height}}]
+          [render-fn window]]]))))
+
+(defn virtual-table [{:keys [class header-count header-renderer row-count row-renderer row-height height window-buffer]
+                      :or {header-count 1 window-buffer 10}}]
+  [:div.virtual-table {:style {:height height}} ; TODO RV not sure why the height is needed as the parent div already has the height set
+   (when header-renderer
+     [:div
+      [:table {:class class}
+       [:thead
+        (for [row-idx (range header-count)]
+          ^{:key row-idx} [header-renderer {:row-idx row-idx}])]]])
+   [window-provider
+    {:item-count row-count :item-height row-height :window-buffer window-buffer}
+    (fn [window]
+      (println window)
+      [:table {:class class}
+       [:tbody
+        (for [row-idx window]
+          ^{:key row-idx} [row-renderer {:style {:height row-height} :row-idx row-idx}])]])]])
--- src/cljs/shevek/components/virtualized_examples.cljs
+++ src/cljs/shevek/components/virtualized_examples.cljs
@@ -6,7 +6,7 @@
 (def lorem "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
 
 (defn example-page []
-  (let [rows 1000
+  (let [rows 100
         columns 3
         rand-str #(-> lorem (str/split #" ") rand-nth)
         data (vec (repeatedly rows #(vec (repeatedly columns rand-str))))]
