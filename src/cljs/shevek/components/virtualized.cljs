2af5a4c83c05453fa24c0cbf9f20055f81f451cc
Emmanuel Nicolau <emmanicolau@gmail.com>
2019-06-18 21:32:31 -0300

    Sync header column widths

--- src/cljs/shevek/components/virtualized.cljs
+++ src/cljs/shevek/components/virtualized.cljs
@@ -23,27 +23,57 @@
             [:div.spacer {:style {:height spacer-height}}]
             [render-fn window]])]))))
 
-(defn virtual-table [{:keys [class header-count header-renderer row-count row-renderer row-height window-buffer]
-                      :or {header-count 1 window-buffer 10}}]
-  [auto-sizer
-   (fn [{:keys [height]}]
-     (let [row-props (fn [row-idx]
-                       {:style {:height row-height} :row-idx row-idx})
-           window-height (- height (* header-count row-height))]
-       [:div.virtual-table {:style {:height height} :class class}
-        (when header-renderer
-          [:div
-           [:table
-            [:thead
-             (for [row-idx (range header-count)]
-               ^{:key row-idx} [header-renderer (row-props row-idx)])]]])
-        [window-provider
-         {:item-count row-count
-          :item-height row-height
-          :window-buffer window-buffer
-          :window-height window-height}
-         (fn [window]
-           [:table
-            [:tbody
-             (for [row-idx window]
-               ^{:key row-idx} [row-renderer (row-props row-idx)])]])]]))])
+(defn- sync-column-widths
+  [vt-node]
+  "As two independent tables are used for headers and content, we need to programmatically fit the header column widths to the content."
+  (when vt-node
+    (let [element (js/$ vt-node)
+          first-content-row (-> element (.find "tbody tr:first") .children .toArray)
+          last-header-row (-> element (.find "thead tr:last") .children .toArray)]
+      (doseq [[content-cell header-cell] (map vector first-content-row last-header-row)
+              :let [content-width (-> content-cell js/$ .width)]
+              :when content-width]
+        (-> header-cell js/$ (.width content-width))))))
+
+(defn headers [{:keys [header-count header-renderer row-height]}]
+  (when header-renderer
+    [:div
+     [:table
+      [:thead
+       (for [row-idx (range header-count)]
+         ^{:key row-idx} [header-renderer {:row-idx row-idx :style {:height row-height}}])]]]))
+
+(defn windowed-content [{:keys [on-change]}]
+  (r/create-class
+   {:component-did-update on-change
+
+    :reagent-render
+    (fn [{:keys [row-renderer row-height window]}]
+      [:table
+       [:tbody
+        (for [row-idx window]
+          ^{:key row-idx} [row-renderer {:row-idx row-idx :style {:height row-height}}])]])}))
+
+(defn virtual-table []
+  (let [vt (r/atom nil)]
+    (fn [{:keys [class header-count header-renderer row-count row-renderer row-height window-buffer]
+          :or {header-count 1 window-buffer 10}}]
+      [auto-sizer
+       (fn [{:keys [height]}]
+         (let [window-height (- height (* header-count row-height))]
+           [:div.virtual-table {:style {:height height}
+                                :class class
+                                :ref #(sync-column-widths (reset! vt %))}
+            [headers {:header-count header-count
+                      :header-renderer header-renderer
+                      :row-height row-height}]
+            [window-provider
+             {:item-count row-count
+              :item-height row-height
+              :window-buffer window-buffer
+              :window-height window-height}
+             (fn [window]
+               [windowed-content {:window window
+                                  :row-renderer row-renderer
+                                  :row-height row-height
+                                  :on-change #(sync-column-widths @vt)}])]]))])))
