b4cb5c074d17f8df2e2243aab88cf3bb94e59052
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-02-16 10:45:10 -0300

    Update some npm packages

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,4 +1,4 @@
 Project.configure do |project|
-  project.email_notifier.emails = %w(emmanicolau@gmail.com sebachavarini@gmail.com)
+  project.email_notifier.emails = %w(emmanicolau@gmail.com)
   project.build_command = 'npm i && DISPLAY=:1 lein ci'
 end
